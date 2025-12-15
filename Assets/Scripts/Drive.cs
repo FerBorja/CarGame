277610aed3d97376e466df057b6cff40b9972ebd
Luis Fernando Casas Borja <luisfernandocasasborja@gmail.com>
2025-12-15 09:34:32 -0600

    Update de carro

--- Assets/Scripts/Drive.cs
+++ Assets/Scripts/Drive.cs
@@ -3,47 +3,66 @@ using UnityEngine.InputSystem;
 
 public class Drive : MonoBehaviour
 {
-    public WheelCollider WC;
+    public WheelCollider[] wheelColliders;
     public float torque = 200;
-    public GameObject wheel;
-    void Start()
-    {
-        if (WC == null)
-        {
-            WC = GetComponent<WheelCollider>();
-        }
-    }
+    public float maxSteerAngle = 30;
+    public GameObject[] wheels;
 
-    void Go(float accel)
+    void Go(float accel, float steer)
     {
-        //Setting de torque
+        //Setting de torque y giro
         accel = Mathf.Clamp(accel, -1f, 1f);
+        steer = Mathf.Clamp(steer, -1f, 1f) * maxSteerAngle;
         float thrustTorque = accel * torque;
-        WC.motorTorque = thrustTorque;
 
-        //Setting de rotación de llantas
-        Quaternion quat;
-        Vector3 position;
-        WC.GetWorldPose(out position, out quat);
-        wheel.transform.position = position;
-        wheel.transform.rotation = quat;
+        for (int i = 0; i < 4; i++)
+        {
+            wheelColliders[i].motorTorque = thrustTorque;
+
+            if (i < 2)
+            {
+                wheelColliders[i].steerAngle = steer;
+            }
+
+            //Setting de rotación de llantas
+            Quaternion quat;
+            Vector3 position;
+            wheelColliders[i].GetWorldPose(out position, out quat);
+            wheels[i].transform.position = position;
+            wheels[i].transform.rotation = quat;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         float a = 0f;
+        float s = 0f;
 
+        //Adelante
         if (Keyboard.current.wKey.isPressed || Keyboard.current.upArrowKey.isPressed)
         {
             a += 1f;
         }
 
+        //Atras
         if (Keyboard.current.sKey.isPressed || Keyboard.current.downArrowKey.isPressed)
         {
             a -= 1f;
         }
 
-        Go(a);
+        //Izquierda
+        if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed)
+        {
+            s -= 1f;
+        }
+
+        //Derecha
+        if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed)
+        {
+            s += 1f;
+        }
+
+        Go(a, s);
     }
 }
